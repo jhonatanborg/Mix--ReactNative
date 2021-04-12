d4acefe0521912e572a2dc65c1b51f378cc4daa4
jhonatanborg <jhonatanborgesdj@gmail.com>
2021-04-12 13:51:31 -0400

    redux persist app config

--- App.js
+++ App.js
@@ -3,23 +3,28 @@ import { NavigationContainer } from "@react-navigation/native";
 import HomeStackNavigator from "./navigations/Navigator";
 import { useFonts } from "expo-font";
 import { ActivityIndicator } from "react-native";
-import { COLORS, FONTS, SIZES } from "./constants/Theme";
-import store from "./store/store";
+import { store, persistor } from "./store/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { AsyncStorage } from "react-native";
+
 const App = () => {
   let [customFonts] = useFonts({
     MulishRegular: require("./assets/fonts/Mulish/static/Mulish-Regular.ttf"),
     MulishBold: require("./assets/fonts/Mulish/static/Mulish-Bold.ttf"),
     MulishSemiBold: require("./assets/fonts/Mulish/static/Mulish-SemiBold.ttf"),
   });
+
   if (!customFonts) {
     return <ActivityIndicator />;
   } else {
     return (
       <Provider store={store}>
-        <NavigationContainer style={{ backgroundColor: "#FFFFFF" }}>
-          <HomeStackNavigator />
-        </NavigationContainer>
+        <PersistGate loading={null} persistor={persistor}>
+          <NavigationContainer style={{ backgroundColor: "#FFFFFF" }}>
+            <HomeStackNavigator />
+          </NavigationContainer>
+        </PersistGate>
       </Provider>
     );
   }
