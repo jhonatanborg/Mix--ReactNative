000d7c04651c32fa7f2cdfefa209a6c6a3a1a0ae
jhonatanborg <jhonatanborgesdj@gmail.com>
2021-04-15 01:15:08 -0400

    details product redesign

--- components/product/HeaderProduct/HeaderProduct.js
+++ components/product/HeaderProduct/HeaderProduct.js
@@ -1,54 +1,57 @@
 import React from "react";
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import Icon from "@expo/vector-icons/Entypo";
 import styles from "./HeaderProduct.style";
+import { convertMoney } from "../../../utils";
 
 const HeaderProduct = (props) => {
   return (
-    <View
-      style={{
-        flexDirection: "row",
-        alignItems: "center",
-        paddingHorizontal: 20,
-        paddingVertical: 20,
-        marginTop: 20,
-      }}
-    >
-      <View style={{ width: "10%" }}>
-        <TouchableOpacity onPress={props.onPress}>
-          <AntDesign name="leftcircleo" size={24} color="black" />
-        </TouchableOpacity>
-      </View>
-      <View style={{ width: "80%", alignItems: "center" }}>
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            alignSelf: "center",
-          }}
-        >
-          <Text
-            style={{
-              paddingHorizontal: 10,
-              fontWeight: "bold",
-              fontSize: 16,
-            }}
-          >
-            {props.categorie}
-          </Text>
-        </View>
-      </View>
-      <View style={{ width: "10%" }}>
-        <Icon name="heart" color="#f9dd7a" size={30} />
-      </View>
-      <Image
+    <View>
+      <ImageBackground
         style={styles.image}
         resizeMode={"cover"}
         source={{
           uri: "https://server.mixentregas.com.br" + props.product.img,
         }}
-      />
+      >
+        <View style={styles.headerTop}>
+          <TouchableOpacity style={styles.backButton} onPress={props.onPress}>
+            <AntDesign name="left" size={24} color="#FFFFFF" />
+          </TouchableOpacity>
+          <View style={{ width: "80%", alignItems: "center" }}>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                alignSelf: "center",
+              }}
+            ></View>
+          </View>
+          <View>
+            <Icon name="heart" color="#f9dd7a" size={30} />
+          </View>
+        </View>
+      </ImageBackground>
+      <View style={styles.details}>
+        <View style={styles.content}>
+          <Text numberOfLines={2} style={styles.title}>
+            {props.product.name}
+          </Text>
+          <Text style={styles.saleValue}>
+            {convertMoney(props.product.sale_value)}
+          </Text>
+        </View>
+        <View>
+          <Text style={styles.description}>{props.product.description}</Text>
+        </View>
+      </View>
     </View>
   );
 };
