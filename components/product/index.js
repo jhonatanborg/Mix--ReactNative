cda90a77b3885aefc8a8eb6d9ce6e94fd716fa9b
jhonatanborg <jhonatanborgesdj@gmail.com>
2021-04-15 01:14:22 -0400

    import component js file

--- components/product/index.js
+++ components/product/index.js
@@ -1,4 +1,5 @@
 import CardProduct from "./CardProduct/CardProduct";
 import ListComplements from "./ListComplements/ListComplements";
 import HeaderProduct from "./HeaderProduct/HeaderProduct";
-export { CardProduct, ListComplements, HeaderProduct };
+import BottomBarSection from "./BottomBarSection/BottomBarSection";
+export { CardProduct, ListComplements, HeaderProduct, BottomBarSection };
